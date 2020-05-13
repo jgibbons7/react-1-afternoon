13b021a78d9d5f76b6d47f25e1c8f1220359d41b
Joshua <gibbons.josh@outlook.com>
2020-05-13 09:10:29 -0600

    Updated

---
+++ src/components/Topics/FilterObject.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class FilterObject extends Component {
+    render(){
+        return(
+            <div className='puzzleBox filterOabjectPB'>
+                <span className='puzzleText'/>
+            </div>
+            )
+    }
+}
+
+export default FilterObject
